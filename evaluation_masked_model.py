43e3375762d7e5304d013160e158aac5e18fc58a
mingyanghe <346390810@qq.com>
2022-03-08 21:32:03 +0100

    code update

--- evaluation_masked_model.py
+++ evaluation_masked_model.py
@@ -12,23 +12,23 @@ import penman
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
      # Get the device
-    sentence_test,amr_sequences_test=open_dataset(path = "/home/students/he/pythonProject1/test")
-    tokenizer = BartTokenizer.from_pretrained('/home/students/he/pythonProject1/bart_model_sentence',local_files_only=True)
-    model = BartForConditionalGeneration.from_pretrained('/home/students/he/pythonProject1/bart_model_sentence',local_files_only=True)
-    new_amr_list_masked = masking_amr(sentence_test, masking_rate=0.15)
+    sentence_test,amr_sequences_test=open_dataset(path = "/home/students/he/Pretraining_AMR/test")
+    tokenizer = BartTokenizer.from_pretrained('/home/students/he/Pretraining_AMR/bart_model',local_files_only=True)
+    model = BartForConditionalGeneration.from_pretrained('/home/students/he/Pretraining_AMR/bart_model',local_files_only=True)
+    new_amr_list_masked = masking_amr(amr_sequences_test, masking_rate=0.15)
     amr_sequences_test = add_token(amr_sequences_test, sequence_type="amr")
     new_amr_list_masked = add_token(new_amr_list_masked, sequence_type="amr")
-    dataset_test = to_dataset(inpus_sequnce=amr_sequences_test, labels_sequence=new_amr_list_masked,tokenizer=tokenizer,
+    dataset_test = to_dataset(inpus_sequnce= new_amr_list_masked, labels_sequence=amr_sequences_test,tokenizer=tokenizer,
                          device=device)
-    tokenizer1 = BartTokenizer.from_pretrained('facebook/bart-base')
-    print("tokenizer_test")
-    text = "i had a relationship with a 16 year old boy and was forbidden from spea"
-    tokenizer_test = tokenizer(text, return_tensors='pt', max_length=512, truncation=True, padding='max_length',
-                       add_special_tokens=False).to(device)
-    tokenizer_test1 = tokenizer1(text, return_tensors='pt', max_length=512, truncation=True, padding='max_length',
-                       add_special_tokens=False).to(device)
-    print(tokenizer1.batch_decode(tokenizer_test1["input_ids"], skip_special_tokens=True, clean_up_tokenization_spaces=True))
-    print(tokenizer.batch_decode(tokenizer_test["input_ids"], skip_special_tokens=True, clean_up_tokenization_spaces=True))
+    # tokenizer1 = BartTokenizer.from_pretrained('facebook/bart-base')
+    # print("tokenizer_test")
+    # text = "i had a relationship with a 16 year old boy and was forbidden from spea"
+    # tokenizer_test = tokenizer(text, return_tensors='pt', max_length=512, truncation=True, padding='max_length',
+    #                    add_special_tokens=False).to(device)
+    # tokenizer_test1 = tokenizer1(text, return_tensors='pt', max_length=512, truncation=True, padding='max_length',
+    #                    add_special_tokens=False).to(device)
+    # print(tokenizer1.batch_decode(tokenizer_test1["input_ids"], skip_special_tokens=True, clean_up_tokenization_spaces=True))
+    # print(tokenizer.batch_decode(tokenizer_test["input_ids"], skip_special_tokens=True, clean_up_tokenization_spaces=True))
 
     loader = torch.utils.data.DataLoader(dataset_test, batch_size=4, shuffle=True)
     model.to(device)
